b3277b0707309e17d05d822b1e27cc836f78343b
ahmed isiaka <ahmeddavids6@gmail.com>
2024-07-18 15:36:14 +0100

    Completed the user onboarding

--- controller/userController.js
+++ controller/userController.js
@@ -225,3 +225,122 @@ exports.resendVerificationEmail = async (req, res) => {
         })
     }
 }
+
+// Forgot Password
+exports.forgotPassword = async (req, res) => {
+    try {
+        // Extract the email from the request body
+        const { email } = req.body;
+
+        // Check if the email exists in the database
+        const user = await UserModel.findOne({ email });
+        if (!user) {
+            return res.status(404).json({
+                message: "User not found"
+            });
+        }
+
+        // Generate a reset token
+        const resetToken = jwt.sign({ email: user.email }, process.env.JWT_SECRET, { expiresIn: "30m" });
+
+        // Send reset password email
+        const mailOptions = {
+            email: user.email,
+            subject: "Password Reset",
+            html: `Please click on the link to reset your password: <a href="${req.protocol}://${req.get("host")}/api/v1/user/reset-password/${resetToken}">Reset Password</a> link expires in 30 minutes`,
+        };
+        //   Send the email
+        await sendMail(mailOptions);
+        //   Send a success response
+        res.status(200).json({
+            message: "Password reset email sent successfully"
+        });
+    } catch (error) {
+        res.status(500).json({
+            message: error.message
+        });
+    }
+};
+
+
+// Reset Password
+exports.resetPassword = async (req, res) => {
+    try {
+        const { token } = req.params;
+        const { password } = req.body;
+
+        // Verify the user's token and extract the user's email from the token
+        const { email } = jwt.verify(token, process.env.JWT_SECRET);
+
+        // Find the user by ID
+        const user = await UserModel.findOne({ email });
+        if (!user) {
+            return res.status(404).json({
+                message: "User not found"
+            });
+        }
+
+        // Salt and hash the new password
+        const saltedRound = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, saltedRound);
+
+        // Update the user's password
+        user.password = hashedPassword;
+        // Save changes to the database
+        await user.save();
+        // Send a success response
+        res.status(200).json({
+            message: "Password reset successful"
+        });
+    } catch (error) {
+        res.status(500).json({
+            message: error.message
+        });
+    }
+};
+
+
+// Change Password
+exports.changePassword = async (req, res) => {
+    try {
+        const { token } = req.params;
+        const { password, existingPassword } = req.body;
+
+        // Verify the user's token and extract the user's email from the token
+        const { email } = jwt.verify(token, process.env.JWT_SECRET);
+
+        // Find the user by ID
+        const user = await UserModel.findOne({ email });
+        if (!user) {
+            return res.status(404).json({
+                message: "User not found"
+            });
+        }
+
+        // Confirm the previous password
+        const isPasswordMatch = await bcrypt.compare(existingPassword, user.password);
+        if (!isPasswordMatch) {
+            return res.status(401).json({
+                message: "Existing password does not match"
+            });
+        }
+
+        // Salt and hash the new password
+        const saltedRound = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, saltedRound);
+
+        // Update the user's password
+        user.password = hashedPassword;
+        // Save the changes to the database
+        await user.save();
+        //   Send a success response
+        res.status(200).json({
+            message: "Password changed successful"
+        });
+    } catch (error) {
+        res.status(500).json({
+            message: error.message
+        });
+    }
+};
+
--- router/userRouter.js
+++ router/userRouter.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { signUp, logIn, scoreStudent, makeAdmin, verifyEmail, resendVerificationEmail } = require('../controller/userController');
+const { signUp, logIn, scoreStudent, makeAdmin, verifyEmail, resendVerificationEmail, forgotPassword, resetPassword, changePassword } = require('../controller/userController');
 const { signUpValidator, logInValidator } = require('../middleware/validator');
 const { authenticate } = require('../middleware/authentication');
 
@@ -16,5 +16,11 @@ router.put('/make-admin/:id', makeAdmin);
 router.get('/verify/:token', verifyEmail);
 
 router.post('/resend-verification', resendVerificationEmail);
+// Forgot password
+router.post('/forgot-password', forgotPassword);
+// Reset password
+router.post('/reset-password/:token', resetPassword);
+// Change password
+router.post('/change-password/:token', changePassword);
 
 module.exports = router
